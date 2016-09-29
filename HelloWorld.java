8e910cc3fb0a5e23ffaa90de9d128afc79e08b82
Simon Readman <sreadman@pd13041.companieshouse.local>
2016-09-29 11:57:03 +0100

    test file 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello Earth!");
+        System.out.println("I love Conflicts!");
+        System.out.println("this is sooooo much fun!");
     }
 }
